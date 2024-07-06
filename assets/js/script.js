6e9ae2c30adb2e7e7cd0c8e0e8aee8147fdf32a9
Marly Valcin <marlyvalcin@gmail.com>
2024-07-05 22:57:41 -0400

    added the script file to index.html and created the function to get the coordinates and added an even listener for the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-const searchInput = document.querySelector('.search-input');
+const city = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
 const atlanta = document.querySelector('.atlanta');
 const denver = document.querySelector('.denver');
 const seattle = document.querySelector('.seattle');
@@ -7,3 +8,21 @@ const orlando = document.querySelector('.orlando');
 const newYork = document.querySelector('.new-york');
 const chicago = document.querySelector('.chicago');
 const austin = document.querySelector('.austin');
+const apiKey = `c56024321501e8e5ba43555acb3aab75`
+
+//Get the coordinates of the cities
+const getCoordinates = async () => {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=1&appid=${apiKey}`
+
+    const response = await fetch(url);
+    console.log(data)
+    const data = await response.json();
+    console.log(data); 
+};
+
+
+//Event listener for the search button
+searchBtn.addEventListener("click", (e) => {
+    e.preverntDefault;
+    getCoordinates();
+});
