84a4b0027a5b2b7ad5b8197bd8492047a5420c70
Marly Valcin <marlyvalcin@gmail.com>
2024-07-05 23:36:52 -0400

    added a function to get the forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,17 +12,40 @@ const apiKey = `c56024321501e8e5ba43555acb3aab75`
 
 //Get the coordinates of the cities
 const getCoordinates = async () => {
+    let coordinates;
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city.value}&limit=1&appid=${apiKey}`
 
-    const response = await fetch(url);
-    console.log(data)
-    const data = await response.json();
-    console.log(data); 
+    try {
+        const response = await fetch(url);
+        console.log(response)
+        const data = await response.json();
+        return coordinates = data[0] 
+    } catch (error) {
+        console.error(error.message);
+    }
+    
 };
 
+//Gets the 5 day forecast
+const getForecast = async () => {
+    const coordinateData = await getCoordinates();
+    const lat = coordinateData.lat;
+    const lon = coordinateData.lon;
+
+    const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error.message);
+    }
+    
+};
 
 //Event listener for the search button
 searchBtn.addEventListener("click", (e) => {
     e.preverntDefault;
-    getCoordinates();
+    getForecast();
 });
